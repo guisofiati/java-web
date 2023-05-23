ad3e8bb55cca2c5e2168c1fd35d14736f513c453
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-05-23 15:10:12 -0300

    handler and router

--- spring/webflux/src/main/java/github/guisofiati/webflux/controllers/PlaylistController.java
+++ spring/webflux/src/main/java/github/guisofiati/webflux/controllers/PlaylistController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@RestController
 @RequestMapping(value = "/playlists")
 public class PlaylistController {
 
