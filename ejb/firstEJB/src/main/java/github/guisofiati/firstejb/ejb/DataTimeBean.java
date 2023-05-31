a59c911c5b6dd6b503ae9baa59e922623af443a9
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-05-31 09:45:51 -0300

    EJB 3. Stateful and Singleton

--- ejb/firstEJB/src/main/java/github/guisofiati/firstejb/ejb/DataTimeBean.java
+++ ejb/firstEJB/src/main/java/github/guisofiati/firstejb/ejb/DataTimeBean.java
@@ -5,16 +5,21 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 @Stateless
 public class DataTimeBean implements Serializable {
+    
+    @EJB
+    private CountBean countBean;
     
     public DataTimeBean() {
         System.out.println("Constructor DataTimeBean called");
     }
     
     public String getDataTimeServer() {
+        countBean.increment();
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         return sdf.format(Calendar.getInstance().getTime());
     }
@@ -27,6 +32,14 @@ public class DataTimeBean implements Serializable {
     @PreDestroy // before delete instance call this method
     public void destroy() {
         System.out.println("Method destroy() DataTimeBean called");
+    }
+
+    public CountBean getCountBean() {
+        return countBean;
+    }
+
+    public void setCountBean(CountBean countBean) {
+        this.countBean = countBean;
     }
     
 }
